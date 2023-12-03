483b4e375baef3f5f6589eec5718112ed830d0ec
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2023-12-03 23:22:52 +0600

    final push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,81 @@
-
-
-
 $(document).ready(function () {
-    // Sidebar toggle behavior
-    const toggler = document.querySelector('.btn');
-    toggler.addEventListener('click', function () {
-        document.querySelector('#sidebar').classList.toggle('collapsed');
-    });
-
-    // Tooltip
-    // $('.tteb-popup-content').hide();
-    $('.ellipsis-btn').on('click', function () {
-        $(this).siblings('.tteb-popup-content').toggle();
-    });
-
-    $(document).click(function (event) {
-        if (!$(event.target).closest('.ellipsis-btn').length) {
-            $('.tteb-popup-content').hide();
+    /* =========================================================
+        Sticky Header
+     =========================================================*/
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 200) {
+            $('.header').addClass('sticky');
+            $('.resp-none').addClass('scroll-none');
+        } else {
+            $('.header').removeClass('sticky');
+            $('.resp-none').removeClass('scroll-none');
         }
     });
 
-    // Socila Media Slider
-        $('#social-media-slider').slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            prevArrow: $('#social-media-slider-prev-1'),
-            nextArrow: $('#social-media-slider-next-1'),
-            responsive: [
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 1,
-                    },
+    /* =========================================================
+        Home Marquee Slider
+     =========================================================*/
+
+    $('.slick-marquee').slick({
+        speed: 5000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        centerMode: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        infinite: true,
+        initialSlide: 1,
+        arrows: false,
+        buttons: false,
+    });
+
+    /* =========================================================
+        Socila Media Slider
+     =========================================================*/
+    $('#social-media-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('#social-media-slider-prev-1'),
+        nextArrow: $('#social-media-slider-next-1'),
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
                 },
-            ],
-        });
+            },
+        ],
+    });
 
-    // Socila Media Slider
-        $('.about-hero-img-wrapper').slick({
-            dots: true,
-            speed: 300,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-        });
+    /* =========================================================
+        About Slider
+     =========================================================*/
+    $('.about-hero-img-wrapper').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
 
-        // Program Slider
+    /* =========================================================
+        Scroll To Top
+     =========================================================*/
+    $('.scrollToTop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
 
+    $(window).scroll(function () {
+        var scrollToTopBtn = $('.scrollToTop');
+        if ($(window).scrollTop() > 200) {
+            scrollToTopBtn.show();
+        } else {
+            scrollToTopBtn.hide();
+        }
+    });
 });
