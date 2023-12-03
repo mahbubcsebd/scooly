08f8f7e517aaafe365b8fd85370e3a611078301d
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2023-12-03 13:45:33 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,34 @@ $(document).ready(function () {
             $('.tteb-popup-content').hide();
         }
     });
+
+    // Socila Media Slider
+        $('#social-media-slider').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: $('#social-media-slider-prev-1'),
+            nextArrow: $('#social-media-slider-next-1'),
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
+
+    // Socila Media Slider
+        $('.about-hero-img-wrapper').slick({
+            dots: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        });
+
+        // Program Slider
+
 });
